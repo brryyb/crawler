574acae4fdb0bbf5ecbae02a92769a94f4bfceca
brryyb <1151596323@qq.com>
2021-04-24 19:41:29 +0800

    17th

--- f05.py
+++ f05.py
@@ -18,9 +18,14 @@ def getLink(fname,links):
           links.append(hlink[i])
           #print (i,len(hlink))
 links = []
+llink = ''
 getLink('f.html',links)
 getLink('f2.html',links)
 getLink('f3.html',links)
 #print(links)
 for i in range(0,len(links)):
     print(i,links[i])
+    llink = links[i]+'\r\n'+llink
+fo = open('flink.txt','w')
+fo.write(llink)
+fo.close()
